4a842610fbcd862e794e5864e598268b01851414
Maximilien Garcia <maximilien.garcia@gmail.com>
2016-06-06 17:37:08 +0200

    Babel and ping command

--- ninibot.js
+++ ninibot.js
@@ -3,25 +3,30 @@
 **/
 'use strict'
 var Discord = require("discord.js");
-import * as login from "../src/admin/login"
+import * as login from "./src/admin/login"
+import * as ping from "./src/command/ping"
 
 
 
 var mybot = new Discord.Client();
+
 login.login(mybot);
 login.logout(mybot);
 
 mybot.on("message", function(message) {
-    if(message.content === "ping") {
-        mybot.reply(message, "pong");
+  var text = message.content;
+  console.log(text);
+  if (message.author != mybot.user && /^!\w*/.test(text)) {
+    var commandCalled = text.split(/\s/);
+    if (commandCalled[0] === "!ping") {
+      ping.ping(mybot, message);
     }
+  }
 });
 
-mybot.on("message", function(message) {
-    var mentions = message.mentions;
-    for(var i = 0; i < mentions.length; i++) {
-      let user = mentions[i];
-      console.log(user.username);
-      mybot.sendMessage(user, "test");
-    }
-})
+/*var mentions = message.mentions;
+for(var i = 0; i < mentions.length; i++) {
+  let user = mentions[i];
+  console.log(user.username);
+  mybot.sendMessage(user, "test");
+}*/
---
+++ src/command/ping.js
@@ -0,0 +1,5 @@
+'use strict'
+
+export function ping(bot, message) {
+  bot.reply(message, "pong");
+}
