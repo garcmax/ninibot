684ec08421f808530cc1b291049084923e2bbbac
Maximilien Garcia <maximilien.garcia@gmail.com>
2016-06-07 15:20:15 +0200

    sending pm only when user is not online

--- src/admin/log.js
+++ src/admin/log.js
@@ -1,5 +1,5 @@
 export function LOG(log, message) {
   if (process.env.NODE_ENV == 'DEBUG') {
-    console.log(`${message.author.username}@${message.channel.name}@${message.channel.server.name} ### ${log}`);
+    console.log(`${message.author.username}@${message.channel.name} ### ${log}`);
   }
 }
--- src/command/ping.js
+++ src/command/ping.js
@@ -10,7 +10,10 @@ export function notif(bot, message) {
   var mentions = message.mentions;
   for(var i = 0; i < mentions.length; i++) {
     let user = mentions[i];
-    LOGGER.LOG(user.username, message);
-    bot.sendMessage(user, `Hello ${user.username}, ${message.author.username} has notified you in this message : "${message.content}"`);
+    LOGGER.LOG(`username to send pm : ${user.username}`, message);
+    LOGGER.LOG(`status of user : ${user.status}`, message);
+    if (user.status != 'online') {
+      bot.sendMessage(user, `Hello ${user.username}, ${message.author.username} has notified you in this message : "${message.content}"`);
+    }
   }
 }
