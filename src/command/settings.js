cc7f903c43a873c00b5747cfe155d1d5df3bc768
Maximilien Garcia <maximilien.garcia@gmail.com>
2016-06-08 15:14:35 +0200

    fixing a crash when ninibot tried to switch to a non learnt language

--- src/command/settings.js
+++ src/command/settings.js
@@ -4,11 +4,15 @@ import * as config from "../admin/config";
 var i18n = new config.I18N();
 
 export function changeLanguage(bot, message) {
-  i18n.language = message.content.substr(6);
-
-  bot.sendMessage(message, config.strings[i18n.language].i18nOK, function(error, message) {
+  let newLang = message.content.substr(6);
+  if (config.strings[newLang]) {
+    i18n.language = newLang;
+    bot.sendMessage(message, config.strings[i18n.language].i18nOK, function(error, message) {
       if (error) {
         bot.reply(message, config.strings[i18n.language].i18nKO);
       }
-  });
+    });
+  } else {
+    bot.reply(message, config.strings[i18n.language].i18nKO);
+  }
 }
