1075393b870c5b4406af327488f4c31f5a9e6911
Maximilien Garcia <niwamaxou@gmail.com>
2016-06-14 22:36:00 +0200

    encoding query

--- src/command/r34.js
+++ src/command/r34.js
@@ -4,7 +4,7 @@ var request = require('request');
 import * as LOGGER from "../admin/log"
 
 export function r34(bot, message) {
-    let query = message.content.substr(5);
+    let query = encodeURIComponent(message.content.substr(5));
     console.log(query);
     let options = {
       url: "http://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=1&tags=" + query
