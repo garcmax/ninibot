8ec191c187ee54a974ff9ec730b7b8774c3634be
Maximilien Garcia <niwamaxou@gmail.com>
2016-06-16 22:33:52 +0200

    WIP on dice command, parsing simple dice

--- src/command/dice.js
+++ src/command/dice.js
@@ -4,5 +4,11 @@ import * as LOGGER from "../admin/log"
 import * as config from "../admin/config"
 
 export function dice(bot, message, options) {
-  
+  let total = 0;
+  for (let i = 1; i < options.length; i++) {
+    let dice = options[i];
+    if (/^d[2468]|10|20|100$/.test(dice)) {
+      return dice;
+    }
+  }
 }
--- test/diceSpec.js
+++ test/diceSpec.js
@@ -8,7 +8,14 @@ describe('recognizing dice', function() {
     let bot = {};
     let message = {"content" : "!dice d6"};
     let options = message.content.split(/\s/);
-    dice(bot, message, ["!dice","d6"]).should.be.equal("d6");
+    dice(bot, message, options).should.be.equal("d6");
+    done();
+  });
+  it('should be a d100', function (done) {
+    let bot = {};
+    let message = {"content" : "!dice d100"};
+    let options = message.content.split(/\s/);
+    dice(bot, message, options).should.be.equal("d100");
     done();
   });
 });
