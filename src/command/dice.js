14e801b4a30c9306e89b57e2671b3fc377ecd006
Maximilien Garcia <niwamaxou@gmail.com>
2016-06-17 22:01:00 +0200

    wip on dice, tests and adding bot message

--- src/command/dice.js
+++ src/command/dice.js
@@ -4,25 +4,27 @@ var i18n = new config.I18N();
 import * as LOGGER from "../admin/log"
 import * as config from "../admin/config"
 
-
-export function dice(bot, message, options) {
+export function throwDice(options) {
   let total = 0;
   let engine = random.engines.mt19937().autoSeed();
-  for (let i = 1; i < options.length; i++) {
-    let dice = options[i];
-    if (/^d[2468]|10|20|100$/.test(dice)) {
-      if (dice.length == 2) {
-        let distribution = random.integer(1,6);
-        let result = distribution(engine);
-        return result;
-      } else if (dice.length == 3) {
-
-      } else if (dice.length == 4) {
-        let distribution = random.integer(1,100);
-        let result = distribution(engine);
-        return result;
-      }
-      return dice;
-    }
+  let dice = options[1];
+  if (/^d[2468]|10|12|20|100$/.test(dice)) {
+    let number = dice.substr(1);
+    let distribution = random.integer(1,number);
+    let result = distribution(engine);
+    return result;
   }
+  return 1;
+}
+
+export function dice(bot, message, options) {
+  let result = throwDice(options);
+  if (result != 1) {
+    bot.message(message.channel, result, function (error) {
+      if (error) {
+        LOGGER.LOG(error, message)
+      }
+    });
+  }
+  return 0;
 }
--- test/diceSpec.js
+++ test/diceSpec.js
@@ -3,7 +3,7 @@ var should = require("should");
 var sinon = require("sinon");
 var random = require("random-js");
 
-import {dice} from "../src/command/dice"
+import {throwDice} from "../src/command/dice"
 
 describe('recognizing dice', function() {
   beforeEach(function() {
@@ -18,18 +18,58 @@ describe('recognizing dice', function() {
     random.integer.restore();
   });
 
-  it('should be a d6', function (done) {
-    let bot = {};
-    let message = {"content" : "!dice d6"};
+  it('should throw a d2', function (done) {
+    let message = {"content" : "!dice d2"};
     let options = message.content.split(/\s/);
-    dice(bot, message, options).should.be.equal(4);
+    throwDice(options).should.be.equal(4);
     done();
   });
-  it('should be a d100', function (done) {
-    let bot = {};
+  it('should throw a d4', function (done) {
+    let message = {"content" : "!dice d4"};
+    let options = message.content.split(/\s/);
+    throwDice(options).should.be.equal(4);
+    done();
+  });
+  it('should throw a d6', function (done) {
+    let message = {"content" : "!dice d6"};
+    let options = message.content.split(/\s/);
+    throwDice(options).should.be.equal(4);
+    done();
+  });
+  it('should throw a d8', function (done) {
+    let message = {"content" : "!dice d8"};
+    let options = message.content.split(/\s/);
+    throwDice(options).should.be.equal(4);
+    done();
+  });
+  it('should throw a d10', function (done) {
+    let message = {"content" : "!dice d10"};
+    let options = message.content.split(/\s/);
+    throwDice(options).should.be.equal(4);
+    done();
+  });
+  it('should throw a d12', function (done) {
+    let message = {"content" : "!dice d12"};
+    let options = message.content.split(/\s/);
+    throwDice(options).should.be.equal(4);
+    done();
+  });
+  it('should throw a d20', function (done) {
+    let message = {"content" : "!dice d20"};
+    let options = message.content.split(/\s/);
+    throwDice(options).should.be.equal(4);
+    done();
+  });
+  it('should throw a d100', function (done) {
     let message = {"content" : "!dice d100"};
     let options = message.content.split(/\s/);
-    dice(bot, message, options).should.be.equal(4);
+    throwDice(options).should.be.equal(4);
+    done();
+  });
+  it('should fail and return 1', function (done) {
+    let message = {"content" : "!dice d13"};
+    let options = message.content.split(/\s/);
+    throwDice(options).should.be.equal(1);
     done();
   });
 });
