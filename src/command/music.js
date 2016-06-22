9c7a85fad1633ae043e30fbf8690ae220778a74f
Maximilien Garcia <niwamaxou@gmail.com>
2016-06-22 22:20:38 +0200

    testing playfile()

--- src/command/music.js
+++ src/command/music.js
@@ -1,4 +1,5 @@
 'use strict'
+var fs = require('fs');
 
 export function music(bot, message) {
   console.log(message.channel.server.channels[1]);
@@ -10,12 +11,16 @@ export function music(bot, message) {
     console.log("SUCCESS");
     let url = message.content.substr(7)
     console.log(url);
-    bot.playFile("https://archive.org/download/testmp3testfile/mpthreetest.mp3", function (error) {
-      if (error) {
-        console.log(error);
-        return -1
-      }
-      console.log("PLAYING FILE");
+    bot.playFile("./test.mp3", { volume: 0.5 }, function (error, streamIntent) {
+      streamIntent.on("error", function (error) {
+          console.log("error " + error);
+      });
+      streamIntent.on("time", function (time) {
+          console.log("time " + time);
+      });
+      streamIntent.on("end", function () {
+          console.log("end");
+      });
     });
   });
 }
