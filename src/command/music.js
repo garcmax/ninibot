b8afc933c1aee6d1b037b7bbbd33352a9750da07
Maximilien Garcia <niwamaxou@gmail.com>
2016-06-29 23:35:56 +0200

    rework on djing

--- src/command/music.js
+++ src/command/music.js
@@ -1,27 +1,45 @@
 'use strict'
 var fs = require('fs');
 var youtube = require('ytdl-core');
-
-var vc;
+var i18n = new config.I18N();
+import * as LOGGER from "../admin/log"
+import * as config from "../admin/config"
 
 export function music(bot, message) {
-  vc = message.channel.server.channels[1]
-  console.log(vc);
-  bot.joinVoiceChannel(vc, function (error) {
-    if (error) {
-      console.log(error);
-      return -1;
+  let vc = message.channel.server.channels;
+  let musicChannel;
+  for(let i = 0; i < vc.length; i++) {
+    if (vc[i].name === "Music") {
+      musicChannel = vc[i];
     }
-    console.log("SUCCESS");
-    let url = message.content.substr(7)
-    console.log(url);
+  }
+  if (musicChannel && !bot.voiceConnection) {
+    bot.sendMessage(message.channel, config.strings[i18n.language].voiceConnectionOK, function (error) {
+      if (error) {
+        LOGGER.LOG(error, message)
+      }
+    });
+    bot.joinVoiceChannel(musicChannel, function (error) {
+      if (error) {
+        console.log(error);
+        return -1;
+      }
+      console.log("SUCCESS");
+    });
+    return 0;
+  }
+  bot.sendMessage(message.channel, config.strings[i18n.language].voiceConnectionKO, function (error) {
+    if (error) {
+      LOGGER.LOG(error, message)
+    }
   });
+  return 1;
 }
 
 export function dj(bot, message) {
   if (message.content) {
     let toto = youtube(message.content, {filter: 'audioonly'})
-    bot.voiceConnection.playRawStream(toto, {volume : 1 }, function (error, streamIntent) {
+    bot.voiceConnection.playRawStream(toto, {volume : 0.5 }, function (error, streamIntent) {
       streamIntent.on("error", function (error) {
         console.log("error " + error);
       });
--- test/musicSpec.js
+++ test/musicSpec.js
@@ -3,8 +3,30 @@ var should = require("should");
 
 import {music, dj} from "../src/command/music";
 
+var bot;
+
+
 describe('music', function() {
-  it('should ...', function (done) {
+
+  before(function() {
+      bot = {joinVoiceChannel: function() {},  sendMessage: function() {}};
+  });
+
+  it('should connect to Music channel', function (done) {
+    let message = {"channel": {"server" : {"channels" : [{"name" : "toto"}, {"name" : "Music"}]}}};
+    music(bot, message).should.be.equal(0);
     done();
   });
+  it('should not connect to any channel', function (done) {
+    let message = {"channel": {"server" : {"channels" : [{"name" : "toto"}, {"name" : "Musique"}]}}};
+    music(bot, message).should.be.equal(1);
+    done();
+  });
+  it('should not connect to any channel', function (done) {
+    bot.voiceConnection = "exist";
+    let message = {"channel": {"server" : {"channels" : [{"name" : "toto"}, {"name" : "Music"}]}}};
+    music(bot, message).should.be.equal(1);
+    done();
+  });
+
 });
