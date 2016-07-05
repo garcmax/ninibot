5eb4ef211aa425a236426736462d309a951a6d06
Maximilien Garcia <maximilien.garcia@gmail.com>
2016-07-05 16:38:43 +0200

    trying to make playlist work

--- src/command/music.js
+++ src/command/music.js
@@ -5,6 +5,8 @@ var i18n = new config.I18N();
 import * as LOGGER from "../admin/log"
 import * as config from "../admin/config"
 
+var playList = [];
+
 export function music(bot, message) {
   let vc = message.channel.server.channels;
   let musicChannel;
@@ -25,6 +27,7 @@ export function music(bot, message) {
         return -1;
       }
       console.log("SUCCESS");
+      playMusic(bot);
     });
     return 0;
   }
@@ -36,27 +39,39 @@ export function music(bot, message) {
   return 1;
 }
 
-export function dj(bot, message) {
-  if (message.content) {
-    try {
-      let toto = youtube(message.content, {filter: 'audioonly'})
-      bot.voiceConnection.playRawStream(toto, {volume : 0.3 }, function (error, streamIntent) {
-        streamIntent.on("error", function (error) {
-          console.log("error " + error);
-        });
-        streamIntent.on("time", function (time) {
-          console.log("time " + time);
-        });
-        streamIntent.on("end", function () {
-          console.log("end");
-        });
-      });
-    } catch (e) {
-      console.log(e);
-    } finally {
-      return 0;
-    }
+export function addMusic(options) {
+  playList.push(options[1]);   
+}
 
+export function playMusic(bot) {
+  while (true) {
+    let playing = false;
+    if (finished && playList.length >= 1) {
+      play(bot, playList[0], function (finished) {
+        playing = !finished;
+        playList.shift();
+      });
+    }
   }
-  return 1
+}
+
+function play(bot, url, finished) {  
+  try {
+    let toto = youtube(url, {filter: 'audioonly'})
+    bot.voiceConnection.playRawStream(toto, {volume : 0.3 }, function (error, streamIntent) {
+      streamIntent.on("error", function (error) {
+        console.log("error " + error);
+      });
+      streamIntent.on("time", function (time) {
+        console.log("time " + time);
+      });
+      streamIntent.on("end", function () {
+        console.log("end");
+        finished(true);
+      });
+    });
+  } catch (e) {
+    console.log(e);
+    finished(true);
+  } 
 }
